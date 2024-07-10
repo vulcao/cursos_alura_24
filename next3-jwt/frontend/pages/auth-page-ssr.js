3a897eea4c3797a2253bc23e0d16799be32edcba
vulcao <vulcao@gmail.com>
2024-07-10 13:41:42 -0300

    concluida página 1

---
+++ next3-jwt/frontend/pages/auth-page-ssr.js
@@ -0,0 +1,9 @@
+function AuthPageSSRc(props) {
+  return (
+    <div>
+      <h1>Auth Page SSR</h1>
+      <pre>{JSON.stringify(props, null, 2)}</pre>
+    </div>
+  );
+}
+export default AuthPageSSRc;
--- next3-jwt/frontend/pages/index.js
+++ next3-jwt/frontend/pages/index.js
@@ -1,22 +1,53 @@
+import React from "react";
+import { useRouter } from "next/router";
+
 export default function HomeScreen() {
+  const router = useRouter();
+  const [value, setValue] = React.useState({
+    usuario: "omariosouto",
+    senha: "safepassword",
+  });
+
+  function handleChange(e) {
+    const fieldValue = e.target.value;
+    const fieldName = e.target.name;
+    setValue((currentValue) => {
+      return {
+        ...currentValue,
+        [fieldName]: fieldValue,
+      };
+    });
+  }
+
   return (
     <div>
       <h1>Login</h1>
-      <form>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          //router.push("/auth-page-ssr");
+          router.push("/auth-page-static");
+        }}
+      >
         <input
-          placeholder="Usuário" name="usuario"
-          defaultValue="omariosouto"
+          placeholder="Usuário"
+          name="usuario"
+          value={value.usuario}
+          onChange={handleChange}
         />
         <input
-          placeholder="Senha" name="senha" type="password"
-          defaultValue="safepassword"
+          placeholder="Senha"
+          name="senha"
+          type="password"
+          value={value.senha}
+          onChange={handleChange}
         />
         <div>
-          <button>
-            Entrar
-          </button>
+          <button>Entrar</button>
         </div>
       </form>
+
+      <pre>{JSON.stringify(value, null, 2)}</pre>
     </div>
   );
 }
