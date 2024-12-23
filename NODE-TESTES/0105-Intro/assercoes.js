9c0d73107a14bd15a1a5f244ec33979588b14ca3
vulcao <vulcao@gmail.com>
2024-12-23 15:38:00 -0300

    aulas 1 e 2

---
+++ NODE-TESTES/0105-Intro/assercoes.js
@@ -0,0 +1,10 @@
+// assercao é um tipo espeçifico de validação
+// podem ser verdadeiras ou falsas
+
+if (2 === 2)
+  if (15 > 42)
+    //true
+
+    //false
+
+    ["abacaxi", "banana", "uva"].includes("abacaxi"); //true
