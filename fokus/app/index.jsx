63992e2f71307b6f75cde1f74121c512eba55bb5
vulcao <vulcao@gmail.com>
2025-03-12 17:57:43 -0300

    varios ajustes

--- fokus/app/index.jsx
+++ fokus/app/index.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet,Image, View } from "react-native";
+import { StyleSheet, Image, View, Text, Pressable } from "react-native";
 
 export default function Index() {
   return (
@@ -8,7 +8,16 @@ export default function Index() {
       <Image
         source={require("./pomodoro.png")}
       />
-      <View style={styles.actions} />
+      <View style={styles.actions}>
+        <Text style={styles.timer}>25:00</Text>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Começar</Text>
+        </Pressable>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>Projeto Fictício sem fins comerciais.</Text>
+        <Text style={styles.footerText}>Desenvolvido por Alura.</Text>
+      </View>
     </View>
   );
 }
@@ -28,5 +37,30 @@ const styles = StyleSheet.create({
     borderRadius: 32,
     borderWidth: 2,
     borderColor: "#144480",
-  }
+    gap: 32,
+  },
+  timer: {
+    fontSize: 54,
+    color: "#FFF",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  button: {
+    padding: 8,
+    backgroundColor: "#bb62f4",
+    borderRadius: 32,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "#021123",
+    textAlign: "center",
+  },footer: {
+    width: "80%",
+  },
+  footerText: {
+    fontSize: 12.5,
+    textAlign: "center",
+    color: "#98A0A8",
+  },
+
 });
